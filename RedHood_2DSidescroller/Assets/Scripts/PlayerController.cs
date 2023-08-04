76d113a6267448c2d6edb2529fd61a7ccf391d90
davamegan <davamae.md@gmail.com>
2023-08-03 22:07:53 -0700

    Script for Player Movement

--- RedHood_2DSidescroller/Assets/Scripts/PlayerController.cs
+++ RedHood_2DSidescroller/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float walkSpeed;
+    Vector2 moveInput; // pulls x and y movement (vector 2)
+
+    public bool IsMoving { get; private set; }
+
+    private void Awake() { // if you want something found AS SOON AS POSSIBLE, use void Awake; it starts before the Start function
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +25,15 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context) {
+        moveInput = context.ReadValue<Vector2>(); // x and y movement input
+
+        IsMoving = moveInput != Vector2.zero; // checking to see if player is NOT moving in order to start moving = false vs true
     }
 }
